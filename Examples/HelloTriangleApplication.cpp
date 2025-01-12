bb8e6d19761a6ca3ce15a157d7d0d49ca4fb1c38
Juanxpeke <juanintendo.f11@gmail.com>
2025-01-12 00:31:42 -0300

    Added initial VkInstance creation

--- Examples/HelloTriangleApplication.cpp
+++ Examples/HelloTriangleApplication.cpp
@@ -5,6 +5,8 @@
 #include <stdexcept>
 #include <cstdlib>
 
+#include <vector>
+
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
@@ -20,6 +22,10 @@ public:
     }
 
 private:
+    GLFWwindow *window;
+
+    VkInstance instance;
+
     void initWindow()
     {
         glfwInit();
@@ -35,6 +41,86 @@ private:
 
     void initVulkan()
     {
+        createInstance();
+    }
+
+    void createInstance()
+    {
+        checkExtensions();
+
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        // This struct is not optional and tells the Vulkan driver which global extensions and validation layers we want to use
+        // Global here means that they apply to the entire program and not a specific device, which will become clear in the next few chapters
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        // Vulkan is a platform agnostic API, which means that you need an extension to interface with the window system
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        // The last two members of the struct determine the global validation layers to enable. Just leave these empty for now
+        createInfo.enabledLayerCount = 0;
+
+        // We've now specified everything Vulkan needs to create an instance
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to create instance!");
+        }
+    }
+
+    void checkExtensions()
+    {
+        // The first parameter allows us to filter extensions by a specific validation layer, which we'll ignore for now
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "Available extensions:\n";
+
+        for (const auto &extension : extensions)
+        {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        for (uint32_t i = 0; i < glfwExtensionCount; i++)
+        {
+            bool found = false;
+
+            for (const auto &extension : extensions)
+            {
+                if (strcmp(glfwExtensions[i], extension.extensionName) == 0)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                throw std::runtime_error("Missing required GLFW extension!");
+            }
+        }
     }
 
     void mainLoop()
@@ -47,12 +133,12 @@ private:
 
     void cleanup()
     {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
     }
-
-    GLFWwindow *window;
 };
 
 int main()
